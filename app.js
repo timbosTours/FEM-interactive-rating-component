a6e588b08be5fb3d99b964be1baef7a3fda7a317
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-08-20 18:29:26 +1000

    fixed functionality

--- app.js
+++ app.js
@@ -3,20 +3,16 @@ function showInput() {
 
 }
 
-
-
 function changeStateOne() {
     var stateOne = document.getElementById('card-state-one');
     var submitButton = document.getElementById('submit-btn');
-    
-    submitButton.addEventListener("click", () => { stateOne.style.display = "none" }); 
 
-    console.log("hey im state one");
+    stateOne.style.display = "none";
 }
+
 function changeStateTwo() {
     var stateTwo = document.getElementById('card-state-two');
-    var submitButton = document.getElementById('submit-btn');
-    submitButton.addEventListener("click", () => { stateTwo.style.display = "block" }); 
 
-    console.log("hey im state two");
+    stateTwo.style.display = "block";
 }
+
